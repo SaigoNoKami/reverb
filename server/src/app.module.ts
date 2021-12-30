4b15cc48fb8b6620f1646459048cc93f2d6fc2fd
SoVa-web <oolga200000001@ukr.net>
2021-12-30 16:55:44 +0200

    вроде работает

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,21 +1,18 @@
-/* eslint-disable prettier/prettier */
-
-import {Module} from "@nestjs/common";
-import {TrackModule} from "./track/track.module";
-import {MongooseModule} from "@nestjs/mongoose";
-import {FileModule} from "./file/file.module";
-import * as path from 'path'
-import {ServeStaticModule} from "@nestjs/serve-static";
+import { Module } from '@nestjs/common';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
+import * as path from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-    imports: [
-        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
-        TrackModule,
-        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.bz042.mongodb.net/reverb?retryWrites=true&w=majority'),
-        FileModule
-    ]
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
+    TrackModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.bz042.mongodb.net/reverb?retryWrites=true&w=majority',
+    ),
+    FileModule,
+  ],
 })
-
-export class AppModule{
-}
-
+export class AppModule {}
