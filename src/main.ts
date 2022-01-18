2f14df3f748d15e743a7ae1a3386bd6b4618f88f
SaigoNoKami <91383895+SaigoNoKami@users.noreply.github.com>
2022-01-18 20:57:09 +0200

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ConfigService} from '@nestjs/config';
+import {config} from 'aws-sdk';
+
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule, { cors: true });
+
+    const configService = app.get(ConfigService);
+    
+  config.update({
+    accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
+    region: configService.get('AWS_REGION'),
+  });
+
+    await app.listen(PORT, () => console.log('server started on PORT ' + PORT));
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();
