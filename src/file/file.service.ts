7349d6a1410f8127847bf0833f7ffc6de8559b34
SaigoNoKami <91383895+SaigoNoKami@users.noreply.github.com>
2022-01-18 21:07:13 +0200

    Add files via upload

---
+++ src/file/file.service.ts
@@ -0,0 +1,49 @@
+import {HttpException, HttpStatus, Injectable} from "@nestjs/common";
+import * as path from 'path'
+import {ConfigService} from '@nestjs/config';
+import * as uuid from 'uuid'
+import * as AWS from 'aws-sdk'
+import * as stream from 'stream';
+
+export enum FileType {
+    AUDIO = 'audio',
+    IMAGE = 'image'
+}
+
+@Injectable()
+export class FileService{
+    constructor(
+        private readonly configService: ConfigService
+      ) {}
+    createFile(type: FileType, file): string {
+        try {
+            const fileExtension =  path.extname(file.originalname) 
+            const fileName = uuid.v4() + fileExtension
+
+            const uploadStream = () => {
+                const s3 = new AWS.S3();
+                const pass = new stream.PassThrough();
+                const uploadParams = {
+                    Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
+                    Body: pass,
+                    Key: type + '/' + fileName
+                  }    
+                return {
+                  writeStream: pass,
+                  promise: s3.upload(uploadParams).promise(),
+                };
+              }
+
+              const {writeStream, promise} = uploadStream()
+              const readStream = stream.Readable.from(file.buffer)
+              const pipeline = readStream.pipe(writeStream);
+
+              
+                
+            return type + '/' + fileName
+
+        } catch (e) {
+            throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+}
